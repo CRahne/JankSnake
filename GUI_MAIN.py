0c424615e2089bd6985f66903591646f7275c0c1
CRahne <coleprahne@gmail.com>
2019-10-31 07:24:59 -0500

    Did some more

--- GUI_MAIN.py
+++ GUI_MAIN.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import keyboard
 
 root = Tk()
 root.geometry('1920x1040+0+0')
@@ -6,34 +7,50 @@ root.geometry('1920x1040+0+0')
 root.configure(bg='white')
 
 mainframe = Frame(root)
-mainframe["bg"] = 'pink'
+mainframe["bg"] = 'black'
 mainframe.grid(sticky=(N, W, E, S))
 
-mainframe.columnconfigure(100, weight=50, minsize=49)
-
-mainframe.rowconfigure(100, weight=50, minsize=49)
+#mainframe.columnconfigure(100, weight=50, minsize=49)
+#
+#mainframe.rowconfigure(100, weight=50, minsize=49)
 
 grid_list = [
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "," "," "]]
-
+    ["N","0","1","2","3","4","5","6","7","8","9"],
+    ["0"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["1"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["2"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["3"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["4"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["5"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["6"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["7"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["8"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],
+    ["9"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "]]
 column_counter = -1
 counter = 0
 for x in grid_list:
     column_counter += 1
     row_counter = 0
     for y in x:
-        print(f'{row_counter} | {column_counter}')
-        grid_list[column_counter][row_counter] = counter
-        this_Label = Label(mainframe, text=grid_list[column_counter][row_counter],bg='blue',padx=20,pady=20)
+        theStringVar = grid_list[column_counter][row_counter]
+        this_Label = Label(mainframe, text=theStringVar,bg='black',padx=10,pady=5)
         this_Label.grid(column=column_counter, row=row_counter)
         counter += 1
         row_counter += 1
+
+def update():
+    column_counter = -1
+    counter = 0
+    for x in grid_list:
+        column_counter += 1
+        row_counter = 0
+        for y in x:
+            theStringVar = grid_list[column_counter][row_counter]
+            if (column_counter == 1 and row_counter == 1) or (column_counter == 1 and row_counter == 2) :
+                this_Label = Label(mainframe, text=theStringVar, bg='yellow',padx=8,pady=2)
+            else:
+                this_Label = Label(mainframe, text=theStringVar,bg='black',fg='orange',padx=10,pady=5)
+            this_Label.grid(column=column_counter, row=row_counter)
+            counter += 1
+            row_counter += 1
+update()
