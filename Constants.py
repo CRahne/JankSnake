bb2ef4897be1bd24c5c45142a6bbd3386b55dba0
CRahne <coleprahne@gmail.com>
2019-11-05 14:37:54 -0600

    Added comments and cleaned up

--- Constants.py
+++ Constants.py
@@ -1,22 +1,42 @@
 
+# The dimensions of the window in pixels
 WindowWidth = 920
 WindowHeight = 920
+# The background color of the window
 WindowBackground = 'white'
 
+# The Button pin numbers
 UpButtonPin = 18
 DownButtonPin = 23
 RightButtonPin = 25
 LeftButtonPin = 24
 
+UpButtonBouncetime = 1000
+DownButtonBouncetime = 1000
+RightButtonBouncetime = 1000
+LeftButtonBouncetime = 1000
+
+# Sets the starting values for the snake
 SnakeHeadX = 5
 SnakeHeadY = 5
 NumberOfSections = 3
 SnakeDirection = 0 # 0 = up, 1 = right, 2 = down, 3 = left
+SnakeHeadColor = 'green'
+SnakeBodyColor = 'yellow'
 
+# The Number of rows/columns that make up the grid
 NumberOfRows = 15
-NumberOfColumns = 10
+NumberOfColumns = 15
 
+# Adds padding(spacing) to the rows and columns
 RowSize = 13
 ColumnSize = 20
 
+
+# Snake Speed in milliseconds
 SnakeSpeed = 250
+# How many milliseconds faster the snake gets each section is gains
+SnakeSpeedAdd = 10
+
+AppleColor = 'red'
+BackgroundColor = 'black'
--- Experimental.py
+++ Experimental.py
@@ -5,25 +5,25 @@ import RPi.GPIO as GPIO
 import random
 import Constants
 
+# Sets up the window
 root = Tk()
 root.geometry(f'{Constants.WindowWidth}x{Constants.WindowHeight}+0+0') # 1920x1040
 root.configure(bg=Constants.WindowBackground)
 
+# Sets the speed of the snake
 Speed = Constants.SnakeSpeed
 
+# Sets the button pin numbers
 UpButton = Constants.UpButtonPin
 DownButton = Constants.DownButtonPin
 RightButton = Constants.RightButtonPin
 LeftButton = Constants.LeftButtonPin
 
-SnakeHeadColor = 'green'
-SnakeBodyColor = 'yellow'
-BackgroundColor = 'black'
+AddSection = False # Checks if a section needs to be added to the snake or not
+GameOver = False # Checks if the snake has run into a wall or itself
+DirectionChanged = False # Checks to see if the direction has been changed
 
-AddSection = False
-GameOver = False
-DirectionChanged = False
-
+# Sets up the starting values for the snake
 NumberOfSections = Constants.NumberOfSections
 HeadX = Constants.SnakeHeadX
 HeadY = Constants.SnakeHeadY
@@ -31,16 +31,16 @@ BodyX = []
 BodyY = []
 SnakeDirection = Constants.SnakeDirection # 0 is up, 1 is right, 2 is down, 3 is left
 
+# Declares variables to be used later
 AppleX = 0
 AppleY = 0
 
-PrevSnakeEndX = 0
-PrevSnakeEndY = 0
+Labels = []
 
-Labels = []# [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-
+# Sets up the GPIO Pins so that button input can be detected
 GPIO.setmode(GPIO.BCM)
 
+# Resets the game once GameOver == True
 def ResetGame():
     global BodyX
     global BodyY
@@ -49,32 +49,45 @@ def ResetGame():
     global GameOver
     global SnakeDirection
     global DirectionChanged
+    global Speed
+    
+    # Pauses the program for 3 seconds
     time.sleep(3)
+    
+    # Removes the snake from the grid
     for x in range(len(BodyX)):
-        Labels[BodyY[x]][BodyX[x]].config(bg = BackgroundColor)
-    # Labels[HeadY][HeadX].config(bg = BackgroundColor)
-    Labels[AppleY][AppleX].config(bg = BackgroundColor)
+        Labels[BodyY[x]][BodyX[x]].config(bg = Constants.BackgroundColor)
+    Labels[AppleY][AppleX].config(bg = Constants.BackgroundColor)
+    
+    # Resets all the snake variables
     HeadX = Constants.SnakeHeadX
     HeadY = Constants.SnakeHeadY
     SnakeDirection = Constants.SnakeDirection
+    Speed = Constants.SnakeSpeed
     SnakeInit()
     DirectionChanged = False
     GenerateApple()
     GameOver = False
 
+
+# Sets up the grid
 def GridInit():
     ColumnCounter = -1
     RowCounter = 0
     for x in range(Constants.NumberOfRows):
+        # Adds another array to Labels for every row in the grid
         Labels.append([])
+        
     for x in range(Constants.NumberOfColumns):
         RowCounter =  0
         ColumnCounter += 1
         for y in range(Constants.NumberOfRows):
-            Labels[RowCounter].insert(ColumnCounter, Label(root, text=" ",bg=BackgroundColor,padx=Constants.ColumnSize,pady=Constants.RowSize))
+            # Adds a blank label to every spot in the grid
+            Labels[RowCounter].insert(ColumnCounter, Label(root, text=" ",bg=Constants.BackgroundColor,padx=Constants.ColumnSize,pady=Constants.RowSize))
             Labels[RowCounter][ColumnCounter].grid(column = ColumnCounter, row = RowCounter)
             RowCounter+=1
 
+# Puts the snake in it's starting position
 def SnakeInit():
     global BodyX
     global BodyY
@@ -82,6 +95,7 @@ def SnakeInit():
     BodyY = []
     for x in range(NumberOfSections):
         if(x != 0):
+            # Sets up the Snake body depending on which way the snake is facing
             if(SnakeDirection == 0):
                 BodyX.append(HeadX)
                 BodyY.append(HeadY + x)
@@ -95,27 +109,34 @@ def SnakeInit():
                 BodyX.append(HeadX + x)
                 BodyY.append(HeadY)
                 
-                
+
+# Draws the snake in the grid
 def DrawSnake(BlackoutY,BlackoutX):
     global AddSection
     global BodyX
     global BodyY
     global GameOver
     global DirectionChanged
+    global Speed
     if(GameOver == False):
         if(AddSection == False):
-            Labels[BlackoutY][BlackoutX].config(bg=BackgroundColor)
+            # Changes the background color of the last snake section back to black
+            Labels[BlackoutY][BlackoutX].config(bg=Constants.BackgroundColor)
         else:
+            # Adds a section to the snake
             BodyX.append(BlackoutX)
             BodyY.append(BlackoutY)
+            Speed -= Constants.SnakeSpeedAdd
             AddSection = False
-        Labels[HeadY][HeadX].config(bg=SnakeHeadColor)
+        # Colors in the new head
+        Labels[HeadY][HeadX].config(bg=Constants.SnakeHeadColor)
+        # Colors in the body
         for x in range(len(BodyX)):
-            Labels[BodyY[x]][BodyX[x]].config(bg=SnakeBodyColor)
-        Labels[PrevSnakeEndY][PrevSnakeEndX].config(bg=BackgroundColor)
+            Labels[BodyY[x]][BodyX[x]].config(bg=Constants.SnakeBodyColor)
         DirectionChanged = False
 
 
+# Changes the Position of the snake based on the direction the snake is moving
 def UpdateSnakePos():
     global HeadX
     global HeadY
@@ -123,8 +144,10 @@ def UpdateSnakePos():
     if(GameOver == False):
         PrevHeadX = HeadX
         PrevHeadY = HeadY
+        # Makes a copy of the previous body coordinates for reference
         PrevBodyX = BodyX.copy()
         PrevBodyY = BodyY.copy()
+        # Changes the head coordinates based on the direction the snake is moving
         if(SnakeDirection == 0):
             HeadY-=1
         elif(SnakeDirection == 1):
@@ -133,6 +156,7 @@ def UpdateSnakePos():
             HeadY+=1
         elif(SnakeDirection == 3):
             HeadX-=1
+        # Changes the body coordinates
         for x in range(len(BodyX)):
                 if(x == 0):
                     BodyX[0] = PrevHeadX
@@ -140,14 +164,19 @@ def UpdateSnakePos():
                 else:
                     BodyX[x] = PrevBodyX[x-1]
                     BodyY[x] = PrevBodyY[x-1]
+        
         CheckSnakePos()
         DrawSnake(PrevBodyY[-1], PrevBodyX[-1])
 
+
+# Creates the main loop that moves the snake
 def RunGame():
     UpdateSnakePos()
     root.after(Speed, RunGame)
     mainloop()
 
+
+# Changes the snake's direction based on button input
 def ButtonHandler(channel):
     global SnakeDirection
     global DirectionChanged
@@ -162,59 +191,68 @@ def ButtonHandler(channel):
             SnakeDirection = 1
     DirectionChanged = True
 
+
+# Checks if the snake has run into an apple, the edge, or itself
 def CheckSnakePos():
     global AddSection
     global GameOver
     if(GameOver == False):
         if HeadX < 0 or HeadX > Constants.NumberOfColumns - 1 or HeadY < 0 or HeadY > Constants.NumberOfRows - 1:
+            # Resets the game
             GameOver = True
             ResetGame()
         if(HeadX == AppleX and HeadY == AppleY):
+            # Generates a new apple and adds a section to the body of the snake
             GenerateApple()
             AddSection = True
         for x in range(len(BodyX)):
             try:
                 if BodyX[x] == HeadX and BodyY[x] == HeadY:
+                    # Resets the game
                     GameOver = True
                     ResetGame()
             except:
                 print("Try failed")
 
+
+# Generates a new apple
 def GenerateApple():
     global AppleX
     global AppleY
     AppleGenerated = False
     while(AppleGenerated == False):
+        # Picks a random coordinates for the apple
         AppleX = random.randint(0,(Constants.NumberOfColumns - 1))
         AppleY = random.randint(0,(Constants.NumberOfRows - 1))
         if not(AppleX == HeadX and AppleY == HeadY):
+            # Checks if the apple coordinates are empty
             for x in range(len(BodyX)):
                 if(AppleX == BodyX[x] and AppleX == BodyY[x]):
                     AppleGenerated = False
                     break
                 else: AppleGenerated = True
-    Labels[AppleY][AppleX].config(bg = 'red')
+    # Colors in the apple
+    Labels[AppleY][AppleX].config(bg = Constants.AppleColor)
         
 
+# Sets up the snake and the grid
 SnakeInit()
 GridInit()
+
+# Starts the game loop after 500 milliseconds
 root.after(500,RunGame)
 root.after(750,GenerateApple)
 
 
+# Adds event catchers for all of the buttons
 GPIO.setup(UpButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(UpButton, GPIO.RISING, callback=ButtonHandler, bouncetime=1000)
+GPIO.add_event_detect(UpButton, GPIO.RISING, callback=ButtonHandler, bouncetime=Constants.UpButtonBouncetime)
 
 GPIO.setup(DownButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(DownButton, GPIO.RISING, callback=ButtonHandler, bouncetime=1000)
+GPIO.add_event_detect(DownButton, GPIO.RISING, callback=ButtonHandler, bouncetime=Constants.DownButtonBouncetime)
 
 GPIO.setup(LeftButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(LeftButton, GPIO.RISING, callback=ButtonHandler, bouncetime=1000)
+GPIO.add_event_detect(LeftButton, GPIO.RISING, callback=ButtonHandler, bouncetime=Constants.LeftButtonBouncetime)
 
 GPIO.setup(RightButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(RightButton, GPIO.RISING, callback=ButtonHandler, bouncetime=1000)
-
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(4, GPIO.RISING, callback=ButtonHandler, bouncetime=1000)
-
-
+GPIO.add_event_detect(RightButton, GPIO.RISING, callback=ButtonHandler, bouncetime=Constants.RightButtonBouncetime)
